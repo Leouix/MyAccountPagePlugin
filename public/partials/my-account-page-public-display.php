89e249b3975a03011d310ccdd4b915c32bdeca6f
leouix <l.starikov@of.ru>
2024-04-18 01:48:29 +0700

    add main tabs

--- public/partials/my-account-page-public-display.php
+++ public/partials/my-account-page-public-display.php
@@ -15,8 +15,38 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 
+<style>
+    #main-tabs {
+        display: flex;
+
+    }
+
+    #main-tabs .main-tabs-item {
+        width: 45px;
+        height: 110px;
+        border: 1px solid #fff;
+        border-radius: 4px;
+        background: #ccc;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #main-tabs .main-tabs-item.active {
+        background: #4e84ff;
+        height: 130px;
+    }
+
+    #main-tabs .main-tabs-item .text {
+        rotate: -90deg;
+        width: 100%;
+        margin-bottom: -35px;
+    }
+
+</style>
+
 <div id="main-tabs">
-    <div class="main-tabs-item">My Comments</div>
-    <div class="main-tabs-item">Users</div>
-    <div class="main-tabs-item">Info</div>
+    <div class="main-tabs-item active"><div class="text">My Comments</div></div>
+    <div class="main-tabs-item"><div class="text">Users</div></div>
+    <div class="main-tabs-item"><div class="text">Info</div></div>
 </div>
